68bf2580491e233effa734156e3e5a0742008f5f
Gilbert <gilbertintabo@gmail.com>
2024-01-31 17:32:12 +0300

    useEffect to control out of window clicks

--- src/SupportEngine/index.js
+++ src/SupportEngine/index.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Avatar from "./Avatar.js";
+import SupportWindow from "./SupportWindow/index.js";
 
 const SupportEngine = () => {
+  const [visible, setVisible] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setVisible(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+
   return (
-    <div>
-      <Avatar />
+    <div ref={ref}>
+      <SupportWindow visible={visible} />
+      <Avatar
+        onClick={() => setVisible(true)}
+        style={{
+          position: "fixed",
+          bottom: "24px",
+          right: "24px",
+        }}
+      />
     </div>
   );
 };
