f16d6a57126a4ed0ab1365e304bb298db1f3224a
Gilbert <gilbertintabo@gmail.com>
2024-01-31 17:29:23 +0300

    Avatar component props added

--- src/SupportEngine/Avatar.js
+++ src/SupportEngine/Avatar.js
@@ -2,9 +2,28 @@ import React, { useState } from "react";
 import { styles } from "./styles";
 
 const Avatar = (props) => {
+  const [hover, setHover] = useState(false);
   return (
-    <div style={{ position: "fixed", bottom: "24px", right: "24px" }}>
-      <div style={{ ...styles.chatWithMeButton }} />
+    <div style={props.style}>
+      <div
+        className="transition-3"
+        style={{
+          ...styles.avatarHello,
+          ...{ opacity: hover ? "1" : "0" },
+        }}
+      >
+        Hey Its Gilbert
+      </div>
+      <div
+        className="transition-3"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        onClick={() => props.onClick && props.onClick()}
+        style={{
+          ...styles.chatWithMeButton,
+          ...{ border: hover ? "1px solid #f9f0ff" : "4px solid #7a39e0" },
+        }}
+      />
     </div>
   );
 };
