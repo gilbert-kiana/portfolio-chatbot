afaa01433b120e599bc829a1feb05457e10b2a96
Gilbert <gilbertintabo@gmail.com>
2024-02-01 13:20:18 +0300

    chat engine component

---
+++ src/SupportEngine/SupportWindow/ChatEngine.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import { ChatEngineWrapper, Socket, ChatFeed } from "react-chat-engine";
+
+const ChatEngine = (props) => {
+  return (
+    <div
+      className="transition-5"
+      style={{
+        height: props.visible ? "100%" : "0%",
+        zIndex: props.visible ? 100 : 0,
+        width: "100%",
+        backgroundColor: "white",
+      }}
+    >
+      {props.visible && (
+        <ChatEngineWrapper>
+          <Socket
+            projectID={process.env.REACT_APP_CE_PROJECT_ID}
+            userName={props.user.email}
+            userSecret={props.user.email}
+          />
+          <ChatFeed activeChat={props.chat.id} />
+        </ChatEngineWrapper>
+      )}
+    </div>
+  );
+};
+
+export default ChatEngine;
