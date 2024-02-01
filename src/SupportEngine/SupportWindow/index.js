670fcb12cc26464445fc42d462f0e1b50f092d94
Gilbert <gilbertintabo@gmail.com>
2024-02-01 13:19:50 +0300

    added chat engine window and passed props

--- src/SupportEngine/SupportWindow/index.js
+++ src/SupportEngine/SupportWindow/index.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { styles } from "../styles";
 import EmailForm from "./EmailForm";
+import ChatEngine from "./ChatEngine";
 
 const SupportWindow = (props) => {
+  const [user, setUser] = useState(null);
+  const [chat, setChat] = useState(null);
+
   return (
     <div
       className="transition-5"
@@ -11,7 +15,13 @@ const SupportWindow = (props) => {
         ...{ opacity: props.visible ? "1" : "0" },
       }}
     >
-      <EmailForm />
+      <EmailForm
+        setUser={(user) => setUser(user)}
+        setChat={(chat) => setChat(chat)}
+        visible={user === null || chat === null}
+      />
+
+      <ChatEngine visible={user !== null && chat !== null} />
     </div>
   );
 };
